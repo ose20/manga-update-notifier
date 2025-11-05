445dc1120216b585206f75797409685af74d3eeb
ose20 <ose20dive@gmail.com>
2025-11-06 00:42:38 +0900

    fix

--- batch/src/bin/app.rs
+++ batch/src/bin/app.rs
@@ -12,5 +12,14 @@ async fn main() {
 
 async fn run() -> Result<()> {
     init_logger()?;
+
+    // registry を作る
+
+    // manga 一覧を取得する
+    // 各 manga について、以下の非同期タスクを作る
+    // check_and_notify を実行する
+    // これを全部待つ(どれかが失敗しても待つ)
+    // 失敗したやつはエラーメッセージだけ通知して終わる
+
     todo!()
 }
